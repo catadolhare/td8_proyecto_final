b967931518fc7bdaedf49795fae25d28452d20b4
Javier Marenco <javier.marenco@utdt.edu>
2025-08-22 15:58:13 -0300

    Instance parameters in command line

--- Packing3D/src/console/EntryPoint.java
+++ Packing3D/src/console/EntryPoint.java
@@ -6,11 +6,35 @@ public class EntryPoint
 {
 	public static void main(String[] args)
 	{
-		Instance instance = new Instance(20, 10, 10, 10, 7, 5);
+		ArgMap argmap = new ArgMap(args);
+		if( argmap.containsArg("-help") )
+		{
+			showParameters();
+			return;
+		}
+		
+		int L = argmap.intArg("-L", 20);
+		int W = argmap.intArg("-W", 10);
+		int H = argmap.intArg("-H", 10);
+		int l = argmap.intArg("-l", 5);
+		int w = argmap.intArg("-w", 5);
+		int h = argmap.intArg("-h", 7);
+		
+		Instance instance = new Instance(L, W, H, l, w, h);
 		Discretization discretization = new Discretization(instance);
 		Box.initialize(instance, discretization);
 		
 		CompleteModel model = new CompleteModel(instance, discretization);
 		model.solve();
+	}
+	
+	private static void showParameters()
+	{
+		System.out.println("  -L [n]     Length of container");
+		System.out.println("  -W [n]     Width of container");
+		System.out.println("  -H [n]     Height of container");
+		System.out.println("  -l [n]     Length of each box");
+		System.out.println("  -w [n]     Width of each box");
+		System.out.println("  -h [n]     Height of each box");
 	}
 }
